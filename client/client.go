58e42130753994a91f28e970f23c8c82d557d6d7
Billy <wjf136@gmail.com>
2023-02-01 12:02:47 -0800

    More cleanup in client

--- client/client.go
+++ client/client.go
@@ -5,10 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/OpsHelmInc/cloudquery/client/services"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/arn"
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
@@ -217,8 +221,8 @@ func getAccountId(ctx context.Context, awsCfg aws.Config) (*sts.GetCallerIdentit
 	return svc.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
 }
 
-func configureAwsClient(ctx context.Context, logger zerolog.Logger, spec *Spec, account Account, stsClient AssumeRoleAPIClient) (aws.Config, error) {
-	var err error
+func configureAwsClient(ctx context.Context, logger zerolog.Logger, awsConfig *Spec, account Account, stsClient AssumeRoleAPIClient) (aws.Config, error) {
+	//var err error
 
 	//maxAttempts := 10
 	//if spec.MaxRetries != nil {
@@ -293,18 +297,104 @@ func configureAwsClient(ctx context.Context, logger zerolog.Logger, spec *Spec, 
 	//	}
 	//}
 
-	if spec.AWSDebug {
-		spec.AWSConfig.ClientLogMode = aws.LogRequestWithBody | aws.LogResponseWithBody | aws.LogRetries
-		spec.AWSConfig.Logger = AwsLogger{logger.With().Str("accountName", account.AccountName).Logger()}
+	//if spec.AWSDebug {
+	//	spec.AWSConfig.ClientLogMode = aws.LogRequestWithBody | aws.LogResponseWithBody | aws.LogRetries
+	//	spec.AWSConfig.Logger = AwsLogger{logger.With().Str("accountName", account.AccountName).Logger()}
+	//}
+
+	//// Test out retrieving credentials
+	//if _, err := spec.AWSConfig.Credentials.Retrieve(ctx); err != nil {
+	//	logger.Error().Err(err).Msg("error retrieving credentials")
+	//	return spec.AWSConfig, errRetrievingCredentials
+	//}
+	var err error
+	var awsCfg aws.Config
+
+	maxAttempts := 10
+	if awsConfig.MaxRetries != nil {
+		maxAttempts = *awsConfig.MaxRetries
+	}
+	maxBackoff := 30
+	if awsConfig.MaxBackoff != nil {
+		maxBackoff = *awsConfig.MaxBackoff
+	}
+
+	configFns := []func(*config.LoadOptions) error{
+		config.WithDefaultRegion(defaultRegion),
+		// https://aws.github.io/aws-sdk-go-v2/docs/configuring-sdk/retries-timeouts/
+		config.WithRetryer(func() aws.Retryer {
+			return retry.NewStandard(func(so *retry.StandardOptions) {
+				so.MaxAttempts = maxAttempts
+				so.MaxBackoff = time.Duration(maxBackoff) * time.Second
+				so.RateLimiter = &NoRateLimiter{}
+			})
+		}),
+	}
+
+	if account.DefaultRegion != "" {
+		// According to the docs: If multiple WithDefaultRegion calls are made, the last call overrides the previous call values
+		configFns = append(configFns, config.WithDefaultRegion(account.DefaultRegion))
+	}
+
+	if account.LocalProfile != "" {
+		configFns = append(configFns, config.WithSharedConfigProfile(account.LocalProfile))
+	}
+
+	awsCfg, err = config.LoadDefaultConfig(ctx, configFns...)
+
+	if err != nil {
+		logger.Error().Err(err).Msg("error loading default config")
+		return awsCfg, err
+	}
+
+	if account.RoleARN != "" {
+		opts := make([]func(*stscreds.AssumeRoleOptions), 0, 1)
+		if account.ExternalID != "" {
+			opts = append(opts, func(opts *stscreds.AssumeRoleOptions) {
+				opts.ExternalID = &account.ExternalID
+			})
+		}
+		if account.RoleSessionName != "" {
+			opts = append(opts, func(opts *stscreds.AssumeRoleOptions) {
+				opts.RoleSessionName = account.RoleSessionName
+			})
+		}
+
+		if stsClient == nil {
+			stsClient = sts.NewFromConfig(awsCfg)
+		}
+		provider := stscreds.NewAssumeRoleProvider(stsClient, account.RoleARN, opts...)
+
+		awsCfg.Credentials = aws.NewCredentialsCache(provider, func(options *aws.CredentialsCacheOptions) {
+			// ExpiryWindow will allow the credentials to trigger refreshing prior to
+			// the credentials actually expiring. This is beneficial so race conditions
+			// with expiring credentials do not cause requests to fail unexpectedly
+			// due to ExpiredToken exceptions.
+			//
+			// An ExpiryWindow of 5 minute would cause calls to IsExpired() to return true
+			// 5 minutes before the credentials are actually expired. This can cause an
+			// increased number of requests to refresh the credentials to occur. We balance this with jitter.
+			options.ExpiryWindow = 5 * time.Minute
+			// Jitter is added to avoid the thundering herd problem of many refresh requests
+			// happening all at once.
+			options.ExpiryWindowJitterFrac = 0.5
+		})
+	}
+
+	if awsConfig.AWSDebug {
+		awsCfg.ClientLogMode = aws.LogRequestWithBody | aws.LogResponseWithBody | aws.LogRetries
+		awsCfg.Logger = AwsLogger{logger.With().Str("accountName", account.AccountName).Logger()}
 	}
 
 	// Test out retrieving credentials
-	if _, err := spec.AWSConfig.Credentials.Retrieve(ctx); err != nil {
+	if _, err := awsCfg.Credentials.Retrieve(ctx); err != nil {
 		logger.Error().Err(err).Msg("error retrieving credentials")
-		return spec.AWSConfig, errRetrievingCredentials
+		return awsCfg, errRetrievingCredentials
 	}
 
-	return spec.AWSConfig, err
+	return awsCfg, err
+
+	//return spec.AWSConfig, err
 }
 
 func CustomConfig(config aws.Config) plugins.SourceNewExecutionClientFunc {
