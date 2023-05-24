bfeff378edd22f32b3b093cd218d5c7962a98f67
Kyle McCullough <kylemcc@gmail.com>
2023-05-24 15:13:54 -0700

    ecr: fix policy fetch
    Signed-off-by: Kyle McCullough <kylemcc@gmail.com>

--- resources/services/ecr/registry_policies_fetch.go
+++ resources/services/ecr/registry_policies_fetch.go
@@ -3,15 +3,18 @@ package ecr
 import (
 	"context"
 
-	"github.com/OpsHelmInc/cloudquery/client"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
 	"github.com/cloudquery/plugin-sdk/schema"
+
+	"github.com/OpsHelmInc/cloudquery/client"
 )
 
-func fetchEcrRegistryPolicies(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
+func fetchEcrRegistryPolicies(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
 	c := meta.(*client.Client)
 	svc := c.Services().Ecr
-	output, err := svc.GetRegistryPolicy(ctx, &ecr.GetRegistryPolicyInput{})
+	output, err := svc.GetRegistryPolicy(ctx, &ecr.GetRegistryPolicyInput{}, func(options *ecr.Options) {
+		options.Region = c.Region
+	})
 	if err != nil {
 		if client.IsAWSError(err, "RegistryPolicyNotFoundException") {
 			return nil
