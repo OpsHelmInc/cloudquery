7a0cf1641b357745cf63a0640608f88d7d7b1126
Kyle McCullough <kylemcc@gmail.com>
2023-09-02 10:49:32 -0700

    add arn to eip
    Signed-off-by: Kyle McCullough <kylemcc@gmail.com>

--- resources/services/ec2/eips.go
+++ resources/services/ec2/eips.go
@@ -25,6 +25,11 @@ func Eips() *schema.Table {
 				Resolver: client.ResolveAWSRegion,
 			},
 			{
+				Name:     "arn",
+				Type:     schema.TypeString,
+				Resolver: resolveEipArn,
+			},
+			{
 				Name:     "oh_resource_type",
 				Type:     schema.TypeString,
 				Resolver: client.StaticValueResolver("AWS::EC2::EIP"),
