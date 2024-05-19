eab104c9091c2111276afa94f30407704649744e
Kyle McCullough <kylemcc@gmail.com>
2024-05-18 18:05:35 -0700

    embed tags in sns topics and sqs queues
    Signed-off-by: Kyle McCullough <kylemcc@gmail.com>

--- resources/services/sns/subscriptions_fetch.go
+++ resources/services/sns/subscriptions_fetch.go
@@ -3,30 +3,29 @@ package sns
 import (
 	"context"
 
-	"github.com/OpsHelmInc/cloudquery/client"
-	"github.com/OpsHelmInc/cloudquery/resources/services/sns/models"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 	"github.com/aws/aws-sdk-go-v2/service/sns/types"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/mitchellh/mapstructure"
+
+	"github.com/OpsHelmInc/cloudquery/client"
+	"github.com/OpsHelmInc/ohaws"
 )
 
 func fetchSnsSubscriptions(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
 	c := meta.(*client.Client)
 	svc := c.Services().Sns
 	config := sns.ListSubscriptionsInput{}
-	for {
-		output, err := svc.ListSubscriptions(ctx, &config)
+	paginator := sns.NewListSubscriptionsPaginator(svc, &config)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx, func(o *sns.Options) {
+			o.Region = c.Region
+		})
 		if err != nil {
 			return err
 		}
-		res <- output.Subscriptions
-
-		if aws.ToString(output.NextToken) == "" {
-			break
-		}
-		config.NextToken = output.NextToken
+		res <- page.Subscriptions
 	}
 	return nil
 }
@@ -35,7 +34,7 @@ func getSnsSubscription(ctx context.Context, meta schema.ClientMeta, resource *s
 	c := meta.(*client.Client)
 	svc := c.Services().Sns
 	item := resource.Item.(types.Subscription)
-	s := models.Subscription{
+	s := ohaws.Subscription{
 		SubscriptionArn: item.SubscriptionArn,
 		Owner:           item.Owner,
 		Protocol:        item.Protocol,
